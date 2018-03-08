c4fa531456efb5c4c5dfa76db7544c9b4e0bf22d
siddhant <siddhantkgarg@gmail.com>
2018-03-08 20:51:21 +0530

    uploader + file server

---
+++ app.js
@@ -0,0 +1,141 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+
+var await = require('await');
+var pdf = require('html-pdf');
+var options = {  };
+ 
+var parseXlsx = require('excel');
+var dataObj = {
+  item : "",
+  quantity:"",
+  cost:"",
+  amountt:"",
+  date:"",
+  id:"",
+  name:"",
+  deliveryStatus:"",
+  paymentstatus:"",
+  deliveryaddress:""
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.post('/upload', function(req, res){
+
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+});
+
+
+app.get('/gr',function(req,res){
+  var response = "";
+    parseXlsx('uploads/orderlist.xlsx', function(err, data) {
+  if(err) throw err;
+  var map = [];
+  data.forEach(function(object){
+    if(object[5]!=="" && object[5]!="ID"){
+      map[object[5]] = map[object[5]] || [];
+      map[object[5]].push(object);  
+    } 
+  });
+
+  map.forEach(function(person,key){
+      var template = "";
+      var sno = 1;
+    var consumer_name="";   
+    var consumer_address="";          
+        var consumer_mobile="";                    
+        var total = 0;
+        var  date="";
+      person.forEach(function(items){
+        date = formatDate(new Date((items[4] - (25567 + 1))*86400*1000));
+        consumer_name = items[6];
+        consumer_address = items[9];
+        consumer_mobile=items[10];
+        total = total + parseFloat(items[3]) ;
+        template+='<tr><td>'+sno+'</td><td>'+items[0]+'</td><td>'+items[1]+'</td><td> &#8377; '+items[2]+'</td><td> &#8377; '+items[3]+'</td></tr>';
+        sno++;
+      });
+      var html = fs.readFileSync('./templates/invoice-template.html','utf8');
+      var result = html.replace("{{consumer-items}}",template);
+      result = result.replace("{{consumer-name}}",consumer_name);
+    result = result.replace("{{consumer-address}}",consumer_address);
+    result = result.replace("{{consumer-mobile}}",consumer_mobile);
+    result = result.replace("{{consumer-total-amount}}",total.toFixed(2));
+    result = result.replace("{{consumer-bill-amount}}",total.toFixed(2));
+
+      //console.log(result) 
+      fs.writeFile('./tmp/invoice-template-'+key+'.html',result,'utf8',function(err,data){
+        var html3 = fs.readFile('./tmp/invoice-template-'+key+'.html','utf8',function(err,data){
+      //console.log(data);
+      console.log("converting to pdf "+ key + person+"\n");
+       pdf.create(data, options).toFile('./uploads/invoices/invoice_'+key+'_'+consumer_name+'_'+date+'.pdf', function(err, res) {
+        if (err) return console.log(err);
+        console.log(res);
+         // { filename: '/app/businesscard.pdf' }
+         response+=(res+"<br/>");
+        // fs.appendFileSync('uploads/list.txt', res);
+    }); 
+    }); 
+      });
+  });
+  
+});
+res.end("Pdfs will be generated shortly. Visit : http://urbanstraw.com:3030 to download files");
+});
+
+app.get('/upload',function(req,res){
+
+})
+var server = app.listen(3000, function(){
+  console.log('Server listening on port 3000');
+});
+
+
+ function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + '_' + monthNames[monthIndex] + '_' + year;
+}
